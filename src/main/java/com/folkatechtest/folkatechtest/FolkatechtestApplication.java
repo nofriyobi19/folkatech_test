bf2c7d34a88e9078523066bb4f466c77894ac5cb
Nofri Yobi <nofriyobi52@gmail.com>
2025-01-22 20:10:33 +0700

    initial project

---
+++ src/main/java/com/folkatechtest/folkatechtest/FolkatechtestApplication.java
@@ -0,0 +1,13 @@
+package com.folkatechtest.folkatechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkatechtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolkatechtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folkatechtest/folkatechtest/FolkatechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folkatechtest.folkatechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkatechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
